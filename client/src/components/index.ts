fde708c92581b6381ec67a1f199d810a55a13e53
Artem Ivanov <ivart.dev@gmail.com>
2022-10-31 15:04:09 +0300

    feat: create Timestamp component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,7 +1,9 @@
 import { App } from "./app/app";
+import { Timestamp } from "./timestamp/timestamp";
 import { Username } from "./username/username";
 
 export {
   App,
-  Username
+  Username,
+  Timestamp,
 }
---
+++ client/src/components/timestamp/timestamp.tsx
@@ -0,0 +1,35 @@
+import React, {DetailedHTMLProps, FC, HTMLAttributes, useEffect, useState} from "react";
+import styles from './timestamp.module.css';
+import cn from "classnames";
+import moment from "moment";
+
+interface ITimestampProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>{
+  time: number,
+  weight?: 'bold' | 'regular'
+}
+
+export const Timestamp: FC<ITimestampProps> =
+  ({ time, weight = 'regular', className, ...props }) => {
+    const [formattedTime, setFormattedTime] = useState<string>(() => moment(time * 1000).fromNow());
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setFormattedTime(moment(time * 1000).fromNow());
+      }, 60 * 1000);
+      return () => {
+        clearInterval(interval);
+      }
+    }, [])
+
+    return (
+      <p
+        className={cn(styles.timestamp, className, {
+          [styles.bold]: weight === 'bold',
+          [styles.regular]: weight === 'regular',
+        })}
+        {...props}
+      >
+        {formattedTime}
+      </p>
+    );
+  }
