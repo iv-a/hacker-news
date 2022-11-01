ff45c403d3f45e1238c14c21bd37a0b8bc7fd68c
Artem Ivanov <ivart.dev@gmail.com>
2022-11-01 19:59:32 +0300

    feat: create getPostWithCommentsController controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import {ENDPOINTS, HN_API_URL} from "../utils/constants";
 import {NextFunction, Request, Response} from "express";
 import DOMPurify from "dompurify";
+import {InternalServerErrorException} from "../exceptions";
+import {IPost} from "../models";
 
 const getItem = async (id: number) => {
   const { data } = await axios.get(HN_API_URL + ENDPOINTS.ITEM + id + ENDPOINTS.JSON);
@@ -12,6 +14,24 @@ const getItem = async (id: number) => {
   }
   return data;
 }
+
+const getTree = async (rootId: number) => {
+  try {
+    const item = await getItem(rootId);
+    const { kids = [], count = 0 } = item;
+
+    const children = await Promise.all(kids.map(async (id: any) => {
+      return await getTree(id);
+    }));
+
+    const filteredChildren = children.filter(({text}) => text);
+    const childrenCount = filteredChildren.reduce((acc, cur) => acc + cur.count, 0);
+
+    return { ...item, kids: filteredChildren, count: count + filteredChildren.length + childrenCount }
+  } catch (err) {
+    throw new InternalServerErrorException();
+  }
+};
 
 export const getPostsListController = async (request: Request, response: Response, next: NextFunction) => {
   try {
@@ -26,6 +46,21 @@ export const getItemController = async (request: Request, response: Response, ne
   const { id } = request.body;
   try {
     response.send(await getItem(id));
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const getPostWithCommentsController = async (request: Request, response: Response, next: NextFunction) => {
+  const { id } = request.body;
+
+  try {
+    const post = await getTree(id);
+    const { kids } = post;
+    response.send({
+      ...post,
+      kids: kids.map(({ kids, ...rest }: IPost) => ({ ...rest })),
+    });
   } catch (err) {
     next(err);
   }
