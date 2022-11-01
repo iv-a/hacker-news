732996d7f44a89324a24e29ab45fc4815bc59539
Artem Ivanov <ivart.dev@gmail.com>
2022-11-01 18:37:25 +0300

    feat: export Exceptions

---
+++ server/src/exceptions/index.ts
@@ -0,0 +1,7 @@
+import { NotFoundException } from "./404-not-found-exception";
+import { InternalServerErrorException } from "./500-internal-server-error";
+
+export {
+  NotFoundException,
+  InternalServerErrorException,
+}
