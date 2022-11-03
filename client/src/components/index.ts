fe787b274b01c71daccc7df86f30675e209c954a
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:50:45 +0300

    feat: add exports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,9 +1,22 @@
-import { App } from "./app/app";
-import { Avatar } from "./avatar/avatar";
-import { Score } from "./score/score";
-import { Timestamp } from "./timestamp/timestamp";
-import { Title } from "./title/title";
-import { Username } from "./username/username";
+import { App } from './app/app';
+import { Avatar } from './avatar/avatar';
+import { Header } from './header/header';
+import { Layout } from './layout/layout';
+import { Main } from './main/main';
+import { NewsCardList } from './news-card-list/news-card-list';
+import { NewsCard } from './news-card/news-card';
+import { NewsPost } from './news-post/news-post';
+import { Paragraph } from './paragraph/paragraph';
+import { Score } from './score/score';
+import { SkeletonPlaceholder } from './skeleton-placeholder/skeleton-placeholder';
+import { Timestamp } from './timestamp/timestamp';
+import { Title } from './title/title';
+import { Username } from './username/username';
+import { Comment } from './comment/comment';
+import { Spinner } from './spinner/spinner';
+import { Button } from './button/button';
+import { Footer } from './footer/footer';
+import { ScrollUp } from './scroll-up/scroll-up';
 
 export {
   App,
@@ -12,4 +25,17 @@ export {
   Timestamp,
   Title,
   Score,
-}
+  NewsCard,
+  SkeletonPlaceholder,
+  Paragraph,
+  Layout,
+  Header,
+  Main,
+  NewsCardList,
+  NewsPost,
+  Comment,
+  Spinner,
+  Button,
+  Footer,
+  ScrollUp,
+};
