56c71827ea82b81cfb27c81e301e73153a989bbd
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:41:19 +0300

    refactor: refactor Title component

--- client/src/components/title/title.tsx
+++ client/src/components/title/title.tsx
@@ -1,18 +1,18 @@
-import React, {DetailedHTMLProps, FC, HTMLAttributes, ReactNode} from "react";
+import React, {
+  DetailedHTMLProps, FC, HTMLAttributes, ReactNode,
+} from 'react';
+import cn from 'classnames';
 import styles from './title.module.css';
-import cn from "classnames";
 
-interface ITitleProps extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>{
+interface ITitleProps extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
   children: ReactNode;
 }
 
-export const Title: FC<ITitleProps> = ({ children, className, ...props }) => {
-  return (
-    <h2
-      className={cn(styles.title, className)}
-      {...props}
-    >
-      {children}
-    </h2>
-  );
-}
+export const Title: FC<ITitleProps> = ({ children, className, ...props }) => (
+  <h2
+    className={cn(styles.title, className)}
+    {...props}
+  >
+    {children}
+  </h2>
+);
