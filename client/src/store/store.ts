9c9510760fc3b77f37dd649836bb8fbb07560939
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:46:25 +0300

    feat: connect reducers

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,17 +1,18 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import hnApi from '../services/api';
+import storiesReducer from './slices/news-stories-slice';
 
 const rootReducer = combineReducers({
+  stories: storiesReducer,
+  [hnApi.reducerPath]: hnApi.reducer,
+});
 
-})
-
-export const configureAppStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-    devTools: process.env.NODE_ENV !== 'production',
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
-  })
-}
+export const configureAppStore = () => configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(hnApi.middleware),
+});
 
 export type TRootState = ReturnType<typeof rootReducer>;
 export type TAppStore = ReturnType<typeof configureAppStore>;
