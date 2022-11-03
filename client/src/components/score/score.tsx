79877be5730975e524f82e328a69777fefd54ba9
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:40:54 +0300

    refactor: refactor Score component

--- client/src/components/score/score.tsx
+++ client/src/components/score/score.tsx
@@ -1,25 +1,30 @@
-import React, {DetailedHTMLProps, FC, HTMLAttributes, useMemo} from "react";
+import React, {
+  DetailedHTMLProps, FC, HTMLAttributes, useMemo,
+} from 'react';
+import cn from 'classnames';
 import styles from './score.module.css';
-import cn from "classnames";
 
-interface IScoreProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>{
+interface IScoreProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
   score: number;
+  size?: 's' | 'l'
 }
 
-export const Score: FC<IScoreProps> = ({ score, className, ...props }) => {
-  const formattedScore = useMemo(() => {
-    return score >= 0 ? '+' + score : '' + score;
-  }, [score]);
+export const Score: FC<IScoreProps> = ({
+  score, size = 'l', className, ...props
+}) => {
+  const formattedScore = useMemo(() => (score >= 0 ? `+${score}` : `${score}`), [score]);
 
   return (
     <p
       className={cn(styles.score, className, {
         [styles.positive]: score >= 0,
         [styles.negative]: score < 0,
+        [styles.l]: size === 'l',
+        [styles.s]: size === 's',
       })}
       {...props}
     >
       {formattedScore}
     </p>
   );
-}
+};
