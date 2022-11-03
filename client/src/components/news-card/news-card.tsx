d0c4d321785e16051db3ce94c1717bc190985638
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:40:21 +0300

    refactor: refactor NewsCard component

--- client/src/components/news-card/news-card.tsx
+++ client/src/components/news-card/news-card.tsx
@@ -1,21 +1,23 @@
-import React, {DetailedHTMLProps, FC, HTMLAttributes} from "react";
+import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 import styles from './news-card.module.css';
-import cn from "classnames";
-import data from './post.json';
-import {Timestamp} from "../timestamp/timestamp";
-import {Avatar} from "../avatar/avatar";
-import {Username} from "../username/username";
-import {Score} from "../score/score";
-import {Title} from "../title/title";
-import {SkeletonPlaceholder} from "../skeleton-placeholder/skeleton-placeholder";
+import { Timestamp } from '../timestamp/timestamp';
+import { Avatar } from '../avatar/avatar';
+import { Username } from '../username/username';
+import { Score } from '../score/score';
+import { Title } from '../title/title';
+import { SkeletonPlaceholder } from '../skeleton-placeholder/skeleton-placeholder';
+import hnApi from '../../services/api';
 
-interface INewsCardProps extends DetailedHTMLProps<HTMLAttributes<HTMLLIElement>, HTMLLIElement>{
+interface INewsCardProps extends DetailedHTMLProps<HTMLAttributes<HTMLLIElement>, HTMLLIElement> {
+  newsId: number;
 }
 
-export const NewsCard: FC<INewsCardProps> = ({ className }) => {
-  const { id, by, title, time, score } = data;
+export const NewsCard: FC<INewsCardProps> = ({ newsId, className }) => {
+  const { data, isLoading } = hnApi.useGetItemQuery(newsId);
 
-  if (!data) {
+  if (isLoading) {
     return (
       <li
         className={cn(styles.card, styles.skeleton, className)}
@@ -25,27 +27,38 @@ export const NewsCard: FC<INewsCardProps> = ({ className }) => {
           <SkeletonPlaceholder />
         </div>
         <div className={cn(styles.skeleton_body)}>
-          <SkeletonPlaceholder className={cn(styles.skeleton_score)}/>
-          <SkeletonPlaceholder className={cn(styles.skeleton_title)}/>
+          <SkeletonPlaceholder className={cn(styles.skeleton_score)} />
+          <SkeletonPlaceholder className={cn(styles.skeleton_title)} />
           <SkeletonPlaceholder />
         </div>
       </li>
     );
   }
 
+  if (!data) {
+    return <></>;
+  }
+
+  const {
+    by, time, score, title, id,
+  } = data;
+
   return (
     <li
       className={cn(styles.card, className)}
     >
-      <div className={cn(styles.card_header)}>
-        <Timestamp time={time} />
-        <Avatar name={by} />
-        <Username>{by}</Username>
-      </div>
-      <div className={cn(styles.card_body)}>
-        <Score score={score} />
-        <Title>{title}</Title>
-      </div>
+      <Link to={{ pathname: `/${id}` }} className={cn(styles.link)}>
+        <div className={cn(styles.card_header)}>
+          <Timestamp time={time} />
+          <Avatar name={by} />
+          <Username>{by}</Username>
+        </div>
+        <div className={cn(styles.card_body)}>
+          <Score score={score} />
+          <Title className={styles.title}>{title}</Title>
+        </div>
+      </Link>
+
     </li>
   );
-}
+};
