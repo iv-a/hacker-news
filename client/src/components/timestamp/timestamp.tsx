464b3f4cf59588918cc1e4b738f6a21cf3a0378b
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:41:06 +0300

    refactor: refactor Timestamp component

--- client/src/components/timestamp/timestamp.tsx
+++ client/src/components/timestamp/timestamp.tsx
@@ -1,35 +1,38 @@
-import React, {DetailedHTMLProps, FC, HTMLAttributes, useEffect, useState} from "react";
+import React, {
+  DetailedHTMLProps, FC, HTMLAttributes, useEffect, useState,
+} from 'react';
+import cn from 'classnames';
+import moment from 'moment';
 import styles from './timestamp.module.css';
-import cn from "classnames";
-import moment from "moment";
 
-interface ITimestampProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>{
+interface ITimestampProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
   time: number,
   weight?: 'bold' | 'regular'
 }
 
-export const Timestamp: FC<ITimestampProps> =
-  ({ time, weight = 'regular', className, ...props }) => {
-    const [formattedTime, setFormattedTime] = useState<string>(() => moment(time * 1000).fromNow());
+export const Timestamp: FC<ITimestampProps> = ({
+  time, weight = 'regular', className, ...props
+}) => {
+  const [formattedTime, setFormattedTime] = useState<string>(() => moment(time * 1000).fromNow());
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setFormattedTime(moment(time * 1000).fromNow());
-      }, 60 * 1000);
-      return () => {
-        clearInterval(interval);
-      }
-    }, [])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFormattedTime(moment(time * 1000).fromNow());
+    }, 60 * 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    return (
-      <p
-        className={cn(styles.timestamp, className, {
-          [styles.bold]: weight === 'bold',
-          [styles.regular]: weight === 'regular',
-        })}
-        {...props}
-      >
-        {formattedTime}
-      </p>
-    );
-  }
+  return (
+    <p
+      className={cn(styles.timestamp, className, {
+        [styles.bold]: weight === 'bold',
+        [styles.regular]: weight === 'regular',
+      })}
+      {...props}
+    >
+      {formattedTime}
+    </p>
+  );
+};
