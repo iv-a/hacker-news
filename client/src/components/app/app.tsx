7b23526103f8e425826a99126cbc115e5d9e495e
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:51:32 +0300

    feat: create app component

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,10 +1,25 @@
-import React, {FC} from "react";
-import styles from "./app.module.css";
+import React, { FC } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  Layout,
+  Header, Footer, ScrollUp,
+} from '..';
+import { MainPage, NewsStoryPage } from '../../pages';
 
-export const App: FC = () => {
-  return (
-    <>
-      <p>Работает</p>
-    </>
-  );
-}
+export const App: FC = () => (
+  <BrowserRouter>
+    <Layout>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <MainPage />
+        </Route>
+        <Route path="/:id">
+          <NewsStoryPage />
+        </Route>
+      </Switch>
+      <Footer />
+      <ScrollUp />
+    </Layout>
+  </BrowserRouter>
+);
