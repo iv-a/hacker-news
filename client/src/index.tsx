e0b82b1e2367737e3e68601b92d1b03d2fec65e7
Artem Ivanov <ivart.dev@gmail.com>
2022-11-03 22:47:22 +0300

    feat: some changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from "./components";
-import {Provider} from "react-redux";
-import {configureAppStore} from "./store/store";
+import { Provider } from 'react-redux';
+import { App } from './components';
+import { configureAppStore } from './store/store';
 
 const store = configureAppStore();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-
